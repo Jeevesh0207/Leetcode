101c2e7ec45e138aff26b6bdc73a97241ca13bf9
Jeevesh Rai <132649861+Jeevesh0207@users.noreply.github.com>
2023-08-30 08:18:37 +0530

    Create  Minimum Replacements to Sort the Array.cpp

---
+++ POTD/Minimum Replacements to Sort the Array.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    long long minimumReplacement(vector<int>& nums) {
+       int n = nums.size(), nxt = 1e9+7;
+       long long ans = 0;
+       for(int i = n-1; i >= 0; i--) {
+           if(nums[i] <= nxt) {
+               nxt = nums[i];
+               continue;
+           }
+           long long parts = ceil(nums[i]/(double)nxt);
+           ans += parts-1;
+           nxt = nums[i]/parts;
+       } 
+       return ans; 
+    }
+};
